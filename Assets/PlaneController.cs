a78e4539742146729bf7fd30d86a918b1c2a1269
nikolaj schlüter.nielsen <nikolaj schlter.nielsen>
2017-01-24 14:43:47 +0100

    Add functional plane

--- Assets/PlaneController.cs
+++ Assets/PlaneController.cs
@@ -1,57 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlaneController : MonoBehaviour {
+public class PlaneController : MonoBehaviour
+{
 	public GameObject player;
 	public GameObject PerlinPlane;
 	public int sizeOfPlane;
 	int oldXChunk;
 	int oldYChunk;
 	GameObject[,] chunks;
-	int xChunk;
-	int yChunk;
+	Vector2 playerInChunk;
 	public int kernelSize = 5;
 	private IEnumerator co;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		float playerXPos = player.transform.position.x;
 		float playerYPos = player.transform.position.z;
-		xChunk = (int) (playerXPos - (playerXPos % sizeOfPlane));
-		yChunk = (int) (playerYPos - (playerYPos % sizeOfPlane));
-		oldXChunk = xChunk;
-		oldYChunk = yChunk;
+		Vector2 playerInChunk = new Vector2 ((playerXPos - (playerXPos % sizeOfPlane)), playerYPos - (playerYPos % sizeOfPlane));
+		oldXChunk = (int)playerInChunk.x;
+		oldYChunk = (int)playerInChunk.y;
 
-		chunks = new GameObject[kernelSize,kernelSize];
+		chunks = new GameObject[kernelSize, kernelSize];
 		co = generateChunks ();
 		StartCoroutine (co);
 	}
 
 	// Update is called once per frame
-	void Update () {
-		float playerXPos = player.transform.position.x;
-		float playerYPos = player.transform.position.z;
-		xChunk = (int) (playerXPos - (playerXPos % sizeOfPlane));
-		yChunk = (int) (playerYPos - (playerYPos % sizeOfPlane));
+	void Update ()
+	{
+		Vector2 playerPos = new Vector2 (player.transform.position.x, player.transform.position.z);
+		playerInChunk = new Vector2 ((int)(playerPos.x - (playerPos.x % sizeOfPlane)), (int)(playerPos.y - (playerPos.y % sizeOfPlane)));
 
-		if (xChunk != oldXChunk || yChunk != oldYChunk) {
-			oldXChunk = xChunk;
-			oldYChunk = yChunk;
+		if (playerInChunk.x != oldXChunk || playerInChunk.y != oldYChunk) {
+			oldXChunk = (int)playerInChunk.x;
+			oldYChunk = (int)playerInChunk.y;
+
+			print ("Player in new chunk");
+
 			co = generateChunks ();
 			StartCoroutine (co);
-			//generateChunks ();
 		}
 
 	}
 
-	private IEnumerator generateChunks(){
+	private IEnumerator generateChunks ()
+	{
+
+		int halfKernel = kernelSize / 2;
 		for (int y = 0; y < kernelSize; y++) {
 			for (int x = 0; x < kernelSize; x++) {
-				//Destroy (chunks [x, y]);
-				Vector3 pos = new Vector3 (xChunk + (x - 2) * sizeOfPlane, 0, yChunk + (y - 2) * sizeOfPlane);
-				chunks[x,y] = (GameObject)Instantiate (PerlinPlane, pos, Quaternion.identity, gameObject.transform);
+				
+				Vector3 currentPlanePos = new Vector3 (playerInChunk.x + (x - halfKernel) * sizeOfPlane, 0, playerInChunk.y + (y - halfKernel) * sizeOfPlane);
+				if (chunks [x, y] == null)
+					chunks [x, y] = (GameObject)Instantiate (PerlinPlane, currentPlanePos, Quaternion.identity, gameObject.transform);
+				
+				//if (Vector2.Distance (chunks [x, y].transform.position, currentPlanePos) > 100) {
+				chunks [x, y].transform.position = currentPlanePos;
+				meshmodify script = chunks [x, y].GetComponent<meshmodify> ();
+
+				script.generatePerlin ();
+				//}
+
 				yield return true;
 			}
 		}
 	}
 
 }
+
+
+//Vector3 pos = new Vector3 (playerInChunk.x + (x - 2) * sizeOfPlane, 0, playerInChunk.y + (y - 2) * sizeOfPlane);
+//if (chunks [x, y] == null)
+//	chunks [x, y] = (GameObject)Instantiate (PerlinPlane, pos, Quaternion.identity, gameObject.transform);
+//
+//print (Vector2.Distance (chunks [x, y].transform.position, playerInChunk));
+//
+//if (Vector2.Distance (chunks [x, y].transform.position, pos) > 100) {
+//	print ("deleting");
+//	Destroy (chunks [x, y]);
+//}
--- Assets/meshmodify.cs
+++ Assets/meshmodify.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class meshmodify : MonoBehaviour {
+public class meshmodify : MonoBehaviour
+{
 	Mesh mesh;
 	Vector3[] vertixes;
 	Vector3[] startVertices;
 
-	[Range (0,0.5f)]
+	[Range (0, 0.5f)]
 	public float density = 2f;
-	[Range (0,100)]
+	[Range (0, 100)]
 	public float height = 2f;
 
 	public bool liveUpdate = false;
@@ -20,30 +21,32 @@ public class meshmodify : MonoBehaviour {
 	float ObjectPosY;
 	public int sizeDensity = 2;
 	// Use this for initialization
-	void Start () {
+	public void Awake ()
+	{
 		mesh = GetComponent<MeshFilter> ().mesh;
 		vertixes = mesh.vertices;
-		print(vertixes.GetLength (0));
-		startVertices = (Vector3[]) mesh.vertices.Clone ();
+//		print(vertixes.GetLength (0));
+		startVertices = (Vector3[])mesh.vertices.Clone ();
 		ObjectPosX = gameObject.transform.position.x;
 		ObjectPosY = gameObject.transform.position.z;
 		generatePerlin ();
 	}
-	
 
 
-	void Update(){
+
+	void Update ()
+	{
 		if (liveUpdate) {
 			generatePerlin ();
 		}
 		if (followPlayer) {
-			gameObject.transform.position = new Vector3(player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
+			gameObject.transform.position = new Vector3 (player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
 		}
 	}
 
 	// Update is called once per frame
-	void generatePerlin () {
-		
+	public void generatePerlin ()
+	{
 		ObjectPosX = gameObject.transform.position.x;
 		ObjectPosY = gameObject.transform.position.z;
 		float seed = 0;
@@ -51,16 +54,16 @@ public class meshmodify : MonoBehaviour {
 			for (float x = 0; x < lenght; x++) {
 				
 				int i = (int)(y * lenght + x);
-				float xP = (x - ObjectPosX/sizeDensity) * density;
-				float yP = (y - ObjectPosY/sizeDensity) * density;
+				float xP = (x - ObjectPosX / sizeDensity) * density;
+				float yP = (y - ObjectPosY / sizeDensity) * density;
 
-				float widePerlin = Mathf.PerlinNoise (xP/8, yP/8);
-				float modifiedHeight = height * widePerlin/2;
-				float perlin = Mathf.PerlinNoise(xP, yP) * modifiedHeight;
-				float perlin2 = Mathf.PerlinNoise(xP*2, yP*2) * modifiedHeight/4;
-				float perlin3 = Mathf.PerlinNoise(xP*4, yP*4) * modifiedHeight/8;
-				float totalPerlin = Mathf.Sin (perlin)+ perlin/1.5f + perlin2 + perlin3 + widePerlin;
-				vertixes [i] = startVertices[i] + new Vector3 (0f, totalPerlin, 0f);
+				float widePerlin = Mathf.PerlinNoise (xP / 8, yP / 8);
+				float modifiedHeight = height * widePerlin / 2;
+				float perlin = Mathf.PerlinNoise (xP, yP) * modifiedHeight;
+				float perlin2 = Mathf.PerlinNoise (xP * 2, yP * 2) * modifiedHeight / 4;
+				float perlin3 = Mathf.PerlinNoise (xP * 4, yP * 4) * modifiedHeight / 8;
+				float totalPerlin = Mathf.Sin (perlin) + perlin / 1.5f + perlin2 + perlin3 + widePerlin;
+				vertixes [i] = startVertices [i] + new Vector3 (0f, totalPerlin, 0f);
 			}
 		}
 		
